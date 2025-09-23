44a126066c5c25224dc25aa506570b2515df0d01
Adesh Pol <adeshpol1234@gmail.com>
2025-09-23 23:50:50 +0530

    Add files via upload

---
+++ Collection Framework/ArrayListDemo.java
@@ -0,0 +1,35 @@
+package com.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> mylist = new ArrayList<String>();
+		
+
+		mylist.add("Java");
+		mylist.add("Python");
+		mylist.add("C++");
+		mylist.add(1, "JavaScript");
+
+		System.out.println("List: " + mylist); // [Java, JavaScript, Python, C++]
+		
+		System.out.println("Get(2): " + mylist.get(2)); // Python
+		
+		System.out.println("Size: " + mylist.size()); // 4
+		System.out.println("Contains Java: " + mylist.contains("Java")); // true
+
+		mylist.remove("C++");
+		System.out.println("After removal: " + mylist); // [Java, JavaScript, Python]
+
+		mylist.set(1, "Go");
+		System.out.println("After set: " + mylist); // [Java, Go, Python]
+
+		Collections.sort(mylist);
+		System.out.println("Sorted: " + mylist); // [Go, Java, Python]
+	}
+
+}
